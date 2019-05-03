e835daf25b92b10ebfa3e72dce615c62c964879d
GaryOgrajsek <43636781+GaryOgrajsek@users.noreply.github.com>
2019-05-03 10:30:43 +0200

    Update Solido.cs

--- Solido/Solido/Solido.cs
+++ Solido/Solido/Solido.cs
@@ -1,12 +1,16 @@
-﻿namespace Solido {
+namespace Solido {
     abstract class Solido {
         private double volume;
         private double peso;
-        private int codice;
+        private string codice;
 
-        protected int Codice { get => codice; set => codice = value; }
+        protected string Codice { get => codice; set => codice = value; }
         protected double Volume { get => volume; set => volume = value; }
         protected double Peso { get => peso; set => peso = value; }
+
+        public Solido(string c) {
+            this.Codice = c;
+        }
 
         abstract public void calcolaP(double pc);
         abstract public void calcolaV();
